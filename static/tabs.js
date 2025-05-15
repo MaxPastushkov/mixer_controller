cf67b7cbb0c0074cec86df27823d693956cc7fef
Max Pastushkov <max@pastushkov.com>
2025-05-15 01:16:16 -0700

    Start UI

---
+++ static/tabs.js
@@ -0,0 +1,13 @@
+function openTab(evt, name) {
+    const tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    const tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(name).style.display = "flex";
+    evt.currentTarget.className += " active";
+}
